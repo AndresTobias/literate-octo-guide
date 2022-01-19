86831ef6065a179fc8b61a3abe9540114aad19b6
AndresTobias <94875669+AndresTobias@users.noreply.github.com>
2022-01-19 17:42:25 -0300

    2h:12m del video lista de tareas arranca con el ruter

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from 'src/app/service/ui.service';
+import{Task} from 'src/app/Task'
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
   styleUrls: ['./add-task.component.css']
 })
 export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter;
 text="";
 day="";
 reminder=false;
-  constructor() { }
+showAddTask: boolean = false;
+subscription?: Subscription;
+  constructor(
+    private uiService: UiService
+  ) { 
+    this.subscription = this.uiService.onToggle()
+    .subscribe( value => this.showAddTask = value)
+  }
 
   ngOnInit(): void {
   }
@@ -20,5 +30,9 @@ reminder=false;
     alert("Agregar tarea");
     return
   }
+  const {text,day,reminder} = this
+  const newTask={text,day,reminder}
+  
+  this.onAddTask.emit(newTask);
 }
 }
